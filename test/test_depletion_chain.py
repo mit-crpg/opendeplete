b103b20dbd4e9f6a3ba4a2af0dfa32b90fec402e
Colin Josey <cjosey@mit.edu>
2017-02-14 15:01:03 -0500

    Address issues in PR #9

--- test/test_depletion_chain.py
+++ test/test_depletion_chain.py
@@ -143,56 +143,6 @@ class TestDepletionChain(unittest.TestCase):
         self.assertEqual(mat[1, 2], mat12)
         self.assertEqual(mat[2, 2], mat22)
 
-    def test_form_matrix_via_wrapper(self):
-        """ Using chain_test, and a dummy reaction rate, compute the matrix using wrapper code """
-        # Relies on test_xml_read passing.
-
-        dep = depletion_chain.DepletionChain()
-        dep.xml_read("chains/chain_test.xml")
-
-        cell_ind = {"10000": 0, "10001": 1}
-        nuc_ind = {"A": 0, "B": 1, "C": 2}
-        react_ind = {"fission": 0, "(n,gamma)": 1}
-
-        react = reaction_rates.ReactionRates(cell_ind, nuc_ind, react_ind)
-
-        react["10000", "C", "fission"] = 1.0
-        react["10000", "A", "(n,gamma)"] = 2.0
-        react["10000", "B", "(n,gamma)"] = 3.0
-        react["10000", "C", "(n,gamma)"] = 4.0
-
-        mat = depletion_chain.matrix_wrapper([dep, react, 0])
-        # Loss A, decay, (n, gamma)
-        mat00 = -np.log(2) / 2.36520E+04 - 2
-        # A -> B, decay, 0.6 branching ratio
-        mat10 = np.log(2) / 2.36520E+04 * 0.6
-        # A -> C, decay, 0.4 branching ratio + (n,gamma)
-        mat20 = np.log(2) / 2.36520E+04 * 0.4 + 2
-
-        # B -> A, decay, 1.0 branching ratio
-        mat01 = np.log(2) / 3.29040E+04
-        # Loss B, decay, (n, gamma)
-        mat11 = -np.log(2) / 3.29040E+04 - 3
-        # B -> C, (n, gamma)
-        mat21 = 3
-
-        # C -> A fission, (n, gamma)
-        mat02 = 0.0292737 * 1.0 + 4
-        # C -> B fission
-        mat12 = 0.002566345 * 1.0
-        # Loss C, fission, (n, gamma)
-        mat22 = -1.0 - 4.0
-
-        self.assertEqual(mat[0, 0], mat00)
-        self.assertEqual(mat[1, 0], mat10)
-        self.assertEqual(mat[2, 0], mat20)
-        self.assertEqual(mat[0, 1], mat01)
-        self.assertEqual(mat[1, 1], mat11)
-        self.assertEqual(mat[2, 1], mat21)
-        self.assertEqual(mat[0, 2], mat02)
-        self.assertEqual(mat[1, 2], mat12)
-        self.assertEqual(mat[2, 2], mat22)
-
     def test_nuc_by_ind(self):
         """ Test nuc_by_ind converter function. """
         dep = depletion_chain.DepletionChain()
